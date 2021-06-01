74476b06b57a2e1a1e38844aa5aa056141d4ce0c
Tejas Jadhav <tjadhav@corp.qualys.com>
2021-06-01 20:45:37 +0530

    React-Data-List component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import AdminMembers from './components/Members';
+
+function App() {
+  return (
+    <div className="App">
+      <AdminMembers/>
+    </div>
+  );
+}
+
+export default App;
